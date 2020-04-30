4279217dc27ec5c36119ec0e4812accf572c22c5
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-04-30 18:49:06 -0500

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Shane Lawson on 4/30/20.
+//  Copyright © 2020 Shane Lawson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
